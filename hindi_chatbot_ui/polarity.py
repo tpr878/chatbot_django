47304daef68d45271c4ac683616df9424b620e88
kvmrvn <106005692+kvmrvn@users.noreply.github.com>
2023-02-11 20:11:23 +0530

    Add files via upload

--- hindi_chatbot_ui/polarity.py
+++ hindi_chatbot_ui/polarity.py
@@ -1,7 +1,7 @@
 def polarity(words):
     import sys
     # caution: path[0] is reserved for script path (or '' in REPL)
-    sys.path.insert(1, 'E:\Chatbot remastered')
+    sys.path.insert(1, r"C:\Users\kamra\Documents\AIIT\chatbot_reprise")
     import chad
 
     polarity = chad.total_polarity(words)
--- hindi_chatbot_ui/views.py
+++ hindi_chatbot_ui/views.py
@@ -3,32 +3,272 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from hindi_chatbot_ui.polarity import polarity
+from random import choice  
+import codecs
+import statistics
 
 
 
 # Create your views here.
-sent_list = []
-l1 = []
-l2 = []
+choice_list = []
+bot_list = []
+user_list = []
+emotion_list = [] 
+ 
 def index(request):
     if request.method == "POST":
         message = request.POST['message']
-        l2.append(message)
-        if polarity(message) > 0:
-            l1.append("positive")
-        elif polarity(message) < 0:
-            l1.append("negative")
-        elif polarity(message) == 0.0:
-            l1.append("neutral")
-        if message == "समाप्त":
-            l1.clear() 
-            l2.clear()
-        mylist = zip(l1, l2)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        
-        mylist = zip(l1, l2)
+        user_list.append(message)
 
+        while message != "समाप्त":
+            # if emotion not found in database 
+            if polarity(message)[0] == None:
+                if polarity(message)[1] > 0:
+                    bot_list.append('POSITIVE')
+                
+                elif polarity(message)[1] < 0:
+                    bot_list.append('NEGATIVE')
+                
+                elif polarity(message)[1] == 0:
+                    bot_list.append('NEUTRAL')
+
+
+            # if emotion found in database 
+            else:
+                emotion_list.append(polarity(message)[0][0])
+                    
+                # if polarity is positive 
+                if polarity(message)[1] > 0: 
+                    if polarity(message)[0][0] == 'joy':
+                        
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: JOY, Polarity: POSITIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: JOY, Polarity: POSITIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: JOY, Polarity: POSITIVE, Scale: HIGH''')
+                    
+                    
+                    elif polarity(message)[0][0] == 'trust':
+                        
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: TRUST, Polarity: POSITIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: TRUST, Polarity: POSITIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: TRUST, Polarity: POSITIVE, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'surprise':
+                        
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: POSITIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: POSITIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: POSITIVE, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'interest':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: INTEREST, Polarity: POSITIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: INTEREST, Polarity: POSITIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: INTEREST, Polarity: POSITIVE, Scale: HIGH''')
+                
+
+                # if polarity is negative 
+                elif polarity(message)[1] < 0:
+                    
+                    if polarity(message)[0][0] == 'fear':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEGATIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEGATIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEGATIVE, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'surprise':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEGATIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEGATIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEGATIVE, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'sadness':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEGATIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEGATIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEGATIVE, Scale: HIGH''')
+                        
+                    
+                    elif polarity(message)[0][0] == 'disgust':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEGATIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEGATIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEGATIVE, Scale: HIGH''')
+
+                    
+                    elif polarity(message)[0][0] == 'anger':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEGATIVE, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEGATIVE, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEGATIVE, Scale: HIGH''')
+
+                
+                # if polarity is neutral 
+                elif polarity(message)[1] == 0:
+                    if polarity(message)[0][0] == 'joy':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: JOY, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: JOY, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: JOY, Polarity: NEUTRAL, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'trust':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: TRUST, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: TRUST, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: TRUST, Polarity: NEUTRAL, Scale: HIGH''')
+
+
+                    elif polarity(message)[0][0] == 'fear':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: FEAR, Polarity: NEUTRAL, Scale: HIGH''')
+
+                    
+                    elif polarity(message)[0][0] == 'surprise':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: SURPRISE, Polarity: NEUTRAL, Scale: HIGH''')
+
+                    
+                    elif polarity(message)[0][0] == 'sadness':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: SADNESS, Polarity: NEUTRAL, Scale: HIGH''')
+
+                    
+                    elif polarity(message)[0][0] == 'disgust':
+                        
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: DISGUST, Polarity: NEUTRAL, Scale: HIGH''')
+                    
+
+                    elif polarity(message)[0][0] == 'anger':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: ANGER, Polarity: NEUTRAL, Scale: HIGH''')
+
+                    
+                    elif polarity(message)[0][0] == 'interest':
+
+                        if polarity(message)[0][1] == 1:
+                            bot_list.append('''Emotion: INTEREST, Polarity: NEUTRAL, Scale: LOW''')
+                        
+                        elif polarity(message)[0][1] == 2:
+                            bot_list.append('''Emotion: INTEREST, Polarity: NEUTRAL, Scale: MEDIUM''')
+                        
+                        elif polarity(message)[0][1] == 3:
+                            bot_list.append('''Emotion: INTEREST, Polarity: NEUTRAL, Scale: HIGH''')
+
+
+                
+
+                # bot_list.append(polarity(message)[0][0])
+
+
+            mylist = zip(bot_list, user_list)
+            return HttpResponseRedirect(reverse("index"))
+        
+        if len(emotion_list) > 0:
+            alert = statistics.mode(emotion_list)
+        else:
+            alert = None
+        
+        bot_list.clear() 
+        user_list.clear()
+
+        return HttpResponseRedirect(reverse("index"))
+    
+    else:
+        mylist = zip(bot_list, user_list)
         return render(request, "hindi_chatbot_ui/index.html", {
-            "mylist": mylist
+            "mylist": mylist,
+            "alert": alert
         })       
